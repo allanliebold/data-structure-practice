82159ed1244be817fdaf5b02bdb9fa56f5bf8d2f
Allan Liebold <allan.liebold@gmail.com>
2017-12-11 15:31:22 -0800

    adds remove and display methods to list and writes tests for them.

--- src/linked_list.py
+++ src/linked_list.py
@@ -20,18 +20,61 @@ class LinkedList(object):
 
     def push(self, data):
         """Add a node containing data to the head of the list."""
-        self.head = Node(data, self.head)
+        new_head = Node(data, self.head)
+        self.head = new_head
         self._size += 1
         return
 
     def pop(self):
-        pass
+        """Remove the node at the head of the list."""
+        popped = self.head.data
+        self.head = self.head.next
+        if self.head:
+            self.head.next = None
+        self._size -= 1
+        return popped
 
     def size(self):
-        pass
+        """Return the number of nodes in the list."""
+        return self._size
 
-    def remove(self):
-        pass
+    def __len__(self):
+        """Return size with built in len function."""
+        return self._size
+
+    def search(self, data):
+        """Return the node containing the passed data, if it exists."""
+        curr = self.head
+        while curr:
+            if curr.data == data:
+                return curr
+            curr = curr.next
+        return
+
+    def remove(self, data):
+        """Remove the node with specified data from the list, if it exists."""
+        target = self.search(data)
+        if not target:
+            return
+
+        if target == self.head:
+            return self.pop()
+
+        curr = self.head
+
+        while curr.next != target:
+            curr = curr.next
+
+        curr.next = target.next
+        self._size -= 1
+        return target.data
 
     def display(self):
-        pass
+        """Return a string containing data in the list."""
+        contents = '('
+        curr = self.head
+        while curr.next:
+            contents += str(curr.data) + ', '
+            curr = curr.next
+        contents += str(curr.data) + ')'
+        return contents
--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -31,7 +31,85 @@ def test_pushing_nodes_adds_to_size():
     """Push should increase the size attribute of the list."""
     from linked_list import LinkedList
     ll = LinkedList()
+    for i in range(10):
+        ll.push(i)
+    assert ll._size == 10
+
+
+def test_pop_removes_head():
+    """Pop on list with one node empties it."""
+    from linked_list import LinkedList
+    ll = LinkedList()
+    ll.push(1)
+    assert ll.pop() == 1
+    assert not ll.head
+    assert ll._size == 0
+
+
+def test_valid_search_return():
+    """Search with valid data should return the correct node."""
+    from linked_list import LinkedList
+    ll = LinkedList()
+    for i in range(10):
+        ll.push(i)
+    assert ll.search(5).data == 5
+
+
+def test_valid_search_head_node():
+    """Search with valid data returns correct node if it is the head."""
+    from linked_list import LinkedList
+    ll = LinkedList()
+    ll.push(3)
+    assert ll.search(3) == ll.head
+
+
+def test_invalid_search():
+    """Search with data not in the list returns None."""
+    from linked_list import LinkedList
+    ll = LinkedList()
+    ll.push(2)
+    ll.push(4)
+    ll.push(6)
+    assert ll.search(8) is None
+
+
+def test_remove_valid_node():
+    """Successfully remove the node with the specified data."""
+    from linked_list import LinkedList
+    ll = LinkedList()
+    for i in range(5):
+        ll.push(i)
+    assert ll.remove(2) == 2
+    assert len(ll) == 4
+    assert ll.head.next.data == 3
+
+
+def test_remove_head():
+    """Remove with data in head node pops it from the list."""
+    from linked_list import LinkedList
+    ll = LinkedList()
     ll.push(1)
     ll.push(2)
     ll.push(3)
-    assert ll._size == 3
+    assert ll.remove(3) == 3
+    assert ll.head.data == 2
+    assert len(ll) == 2
+
+
+def test_remove_head_only_node():
+    """Remove the head in a list with only one node."""
+    from linked_list import LinkedList
+    ll = LinkedList()
+    ll.push(1)
+    assert ll.remove(1) == 1
+    assert not ll.head
+    assert len(ll) == 0
+
+
+def test_display():
+    """Display method returns string of data in the list in order."""
+    from linked_list import LinkedList
+    ll = LinkedList()
+    for i in range(1, 11):
+        ll.push(i)
+    assert ll.display() == '(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)'
